e448666fa6369ab7c25b967f9a2b139c17ccee7d
Qiforiss67 <issaitama07@gmail.com>
2026-07-29 23:11:45 +0700

    fix: register AppController in AppModule for root API response and health checks

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, ProductsModule, TransactionsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
